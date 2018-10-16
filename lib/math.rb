c0e51cf0570ccb79461b298b68daca738eaa96d2
Dakota Clark <bannaray95@gmail.com>
2018-10-16 20:37:51 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,26 +1,20 @@
 def addition(5+4)
 end
 
-def subtraction
-  10-5
+def subtraction(10-5)
 end
 
-def division
-  50/2
+def division(50/2)
 end
 
-def multiplication
-  4*30
+def multiplication(4*30)
 end
 
-def modulo
-  34%5
+def modulo(34%5)
 end
 
-def square_root
-  Math.sqrt(81)
+def square_root(Math.sqrt(81))
 end
 
-def order_of_operation
-  7+ ((43* 23)/83)
+def order_of_operation(7+ ((43* 23)/83))
 end
